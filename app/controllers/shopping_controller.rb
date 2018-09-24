bb663397c27beff81738ccee5d69745ff1f016cb
Matmouze <matheusmom@live.com>
2018-09-24 14:31:21 -0300

    primeiro commit

---
+++ app/controllers/shopping_controller.rb
@@ -0,0 +1,17 @@
+class ShoppingController < ApplicationController
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
