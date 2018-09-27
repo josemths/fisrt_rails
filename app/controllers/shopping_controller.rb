8278b7cb77c750980543d0595fa8b8041613bbcb
josemths <jose.matheus@edu.se.df.gov.br>
2018-09-27 14:33:57 -0300

    V_2

--- app/controllers/shopping_controller.rb
+++ app/controllers/shopping_controller.rb
@@ -1,17 +1,39 @@
 class ShoppingController < ApplicationController
+  
+  def index
+    @shops = Shop.all
+  end
+
+  def show
+    @shop = Shop.find_by(id: params['id'] )
+  end
+  
   def new
     @shop = Shop.new
   end
 
   def create
+    shop = Shop.new(permition)
+    if shop.save
+      @msg = "Salvo com sucesso!"
+    else
+      @msg = shop.errors.messages
+    end
   end
 
   def edit
+    @shop = Shop.find_by(id: params['id'] )
   end
 
   def update
   end
 
   def destroy
+  end
+
+private
+
+  def permition
+    params['shop'].permit(:name, :address)
   end
 end
